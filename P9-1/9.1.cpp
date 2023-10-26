ed4ae9079f195ae8a1469740459415b03e5e2091
RAY OTTO <rjotto123@gmail.com>
2023-10-26 13:04:08 -0400

    Every element in order of appearence in the array.

---
+++ P9-1/9.1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+srand((unsigned)time(0));
+int i=0;
+int values[10];
+values[0]= (rand() % 8999) + 999;
+values[1] = (rand() % 8999) + 999;
+values[2] = (rand() % 8999) + 999;
+values[3] = (rand() % 8999) + 999;
+values[4] = (rand() % 8999) + 999;
+values[5] = (rand() % 8999) + 999;
+values[6] = (rand() % 8999) + 999;
+values[7] = (rand() % 8999) + 999;
+values[8] = (rand() % 8999) + 999;
+values[9] = (rand() % 8999) + 999;
+
+while (i < 10) {
+	cout << values[i] << endl;
+	i++;
+}
+
+}
