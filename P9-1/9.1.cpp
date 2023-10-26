a1093a6461394f675107f85d7fc64cb565d07bea
RAY OTTO <rjotto123@gmail.com>
2023-10-26 13:25:44 -0400

    Can now display every element in order of appearence in the array.

--- P9-1/9.1.cpp
+++ P9-1/9.1.cpp
@@ -24,27 +24,4 @@ while (i < 10) {
 	cout << values[i] << " ";
 	i++;
 }
-cout << endl << "Even indices:  	     " << values[1] <<" "<< values[3] << " " << values[5] << " " << values[7] << " " << values[9]<<endl;
-
-cout << "Even values:         ";
-if (values[0]%2==0)
-cout<< values[0]<<" ";
-if (values[1] % 2 == 0)
-cout << values[1] << " ";
-if (values[2] % 2 == 0)
-cout << values[2] << " ";
-if (values[3] % 2 == 0)
-cout << values[3] << " ";
-if (values[4] % 2 == 0)
-cout << values[4] << " ";
-if (values[5] % 2 == 0)
-cout << values[5] << " ";
-if (values[6] % 2 == 0)
-cout << values[6] << " ";
-if (values[7] % 2 == 0)
-cout << values[7] << " ";
-if (values[8] % 2 == 0)
-cout << values[8] << " ";
-if (values[9] % 2 == 0)
-cout << values[9] << " ";
 }
